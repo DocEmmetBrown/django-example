4fbb0c53193005bdf276da073d5c43eff564e240
Guillaume Winter <guillaume.winter@aiven.io>
2024-07-15 15:33:39 +0200

    add fake secrets

--- secrets.py
+++ secrets.py
@@ -2,5 +2,5 @@
 PASSWORD=password
 SECRET=secret
 
-aws_access_key_id=AKIAIOSFODNN7EEEEE00
-aws_secret_access_key=wJalrXUtnFEMI/K7MDENG/bPxRfiCYeeeee00KEY
+aws_access_key_id=AKIAAAAAAAAAAAAAAAAA
+aws_secret_access_key=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
